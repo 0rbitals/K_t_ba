a4065274fea9960c417d4101aca52a76ec30d5f1
Mahdi <20505824+0rbitals@users.noreply.github.com>
2019-09-12 06:38:36 +0100

    Added styling using Semantic UI, and implemented other functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { Component } from 'react';
+import {
+  Container,
+  Header,
+  Segment,
+} from 'semantic-ui-react';
 import { Editor } from 'slate-react';
 import { Value } from 'slate';
-import './App.css';
 import Buttons from './components/Buttons';
+
+
+const style = {
+  background: '#f4f4f4',
+  border: '1px solid #ddd',
+  borderLeft: '3px solid #06e7c2',
+  color: '#666',
+  pageBreakInside: 'avoid',
+  fontFamily: 'monospace',
+  fontSize: '15px',
+  lineHeight: '1.6',
+  marginBottom: '1.6em',
+  maxWidth: '100%',
+  overflow: 'auto',
+  padding: '1em 1.5em',
+  display: 'block',
+  wordWrap: 'break-word',
+};
 
 const initialValue = Value.fromJSON({
   document: {
@@ -30,7 +52,8 @@ class App extends Component {
       value: initialValue,
     };
 
-    this.buttonClicked = this.buttonClicked.bind(this);
+    this.markClicked = this.markClicked.bind(this);
+    this.blockClicked = this.blockClicked.bind(this);
   }
 
   ref = (editor) => {
@@ -41,10 +64,36 @@ class App extends Component {
     this.setState({ value });
   }
 
-  buttonClicked(event) {
+  markClicked(event) {
     event.preventDefault();
-    const { name } = event.target;
+    let { name } = event.target;
+    if (event.target.type !== 'button') {
+      name = event.target.parentNode.name;
+    }
     this.editor.toggleMark(name);
+  }
+
+  blockClicked(event) {
+    event.preventDefault();
+    const { value } = this.state;
+    const { document } = this.editor.value;
+    let { name } = event.target;
+    if (event.target.type !== 'button') {
+      name = event.target.parentNode.name;
+    }
+    const isList = value.blocks.some((node) => node.type === 'list-item');
+    const isType = value.blocks.some(
+      (block) => document.getClosest(block.key, (parent) => parent.type === name),
+    );
+    if (isList && isType) {
+      this.editor.unwrapBlock(name).setBlocks('paragraph');
+    } else if (isList) {
+      // If this is already a list and you click
+      // on a different type of list button then change the type to that.
+      this.editor.unwrapBlock().wrapBlock(name);
+    } else {
+      this.editor.setBlocks('list-item').wrapBlock(name);
+    }
   }
 
   renderMark(props, editor, next) {
@@ -55,11 +104,28 @@ class App extends Component {
       case 'italic':
         return <em {...{ attributes }}>{children}</em>;
       case 'code':
-        return <code {...{ attributes }}>{children}</code>;
+        return <code style={style} {...{ attributes }}>{children}</code>;
       case 'underline':
         return <u {...{ attributes }}>{children}</u>;
       case 'strikethrough':
         return <s {...{ attributes }}>{children}</s>;
+      case 'h1':
+        return <h1 {...{ attributes }}>{children}</h1>;
+      default:
+        return next();
+    }
+  }
+
+  renderBlock = (props, editor, next) => {
+    const { attributes, children, node } = props;
+
+    switch (node.type) {
+      case 'unorderedList':
+        return <ul {...attributes}>{children}</ul>;
+      case 'list-item':
+        return <li {...attributes}>{children}</li>;
+      case 'orderedList':
+        return <ol {...attributes}>{children}</ol>;
       default:
         return next();
     }
@@ -68,15 +134,27 @@ class App extends Component {
   render() {
     const { value } = this.state;
     return (
-      <div>
-        <Buttons buttonClicked={this.buttonClicked} />
-        <Editor
-          ref={this.ref}
-          value={value}
-          onChange={this.onChange}
-          renderMark={this.renderMark}
-        />
-      </div>
+      <Container text>
+        <Header as="h1" textAlign="center" style={{ marginTop: '20px' }}>K_T_BA Rich Text Editor</Header>
+        <Segment.Group>
+          <Segment>
+            <Buttons
+              markClicked={this.markClicked}
+              value={value}
+              blockClicked={this.blockClicked}
+            />
+          </Segment>
+          <Segment>
+            <Editor
+              ref={this.ref}
+              value={value}
+              onChange={this.onChange}
+              renderMark={this.renderMark}
+              renderBlock={this.renderBlock}
+            />
+          </Segment>
+        </Segment.Group>
+      </Container>
     );
   }
 }
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,21 +1,122 @@
 import React from 'react';
+import { Button, Icon, ButtonGroup } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
 const Buttons = (props) => {
-  const { buttonClicked } = props;
+  const { markClicked, value, blockClicked } = props;
   return (
     <div>
-      <button type="button" name="bold" onMouseDown={buttonClicked}>Bold</button>
-      <button type="button" name="italic" onMouseDown={buttonClicked}>Italic</button>
-      <button type="button" name="code" onMouseDown={buttonClicked}>Code</button>
-      <button type="button" name="underline" onMouseDown={buttonClicked}>Underline</button>
-      <button type="button" name="strikethrough" onMouseDown={buttonClicked}>Strikethrough</button>
+      <Button.Group>
+        <Button
+          toggle
+          active={value.activeMarks.some((mark) => mark.type === 'bold')}
+          icon
+          type="button"
+          name="bold"
+          onMouseDown={markClicked}
+        >
+          <Icon name="bold" />
+        </Button>
+        <Button
+          toggle
+          active={value.activeMarks.some((mark) => mark.type === 'italic')}
+          icon
+          type="button"
+          name="italic"
+          onMouseDown={markClicked}
+        >
+          <Icon name="italic" />
+        </Button>
+        <Button
+          toggle
+          active={value.activeMarks.some((mark) => mark.type === 'underline')}
+          icon
+          type="button"
+          name="underline"
+          onMouseDown={markClicked}
+        >
+          <Icon name="underline" />
+        </Button>
+        <Button
+          toggle
+          active={value.activeMarks.some(
+            (mark) => mark.type === 'strikethrough',
+          )}
+          icon
+          type="button"
+          name="strikethrough"
+          onMouseDown={markClicked}
+        >
+          <Icon name="strikethrough" />
+        </Button>
+        <Button
+          toggle
+          active={value.activeMarks.some((mark) => mark.type === 'code')}
+          icon
+          type="button"
+          name="code"
+          onMouseDown={markClicked}
+        >
+          <Icon name="code" />
+        </Button>
+        <Button
+          toggle
+          active={value.activeMarks.some((mark) => mark.type === 'h1')}
+          icon
+          type="button"
+          name="h1"
+          onMouseDown={markClicked}
+        >
+          <Icon name="heading" />
+        </Button>
+      </Button.Group>
+      {' '}
+      <ButtonGroup>
+        <Button
+          toggle
+          active={value.blocks.some(
+            (block) => value.document.getClosest(block.key, (parent) => parent.type === 'unorderedList'),
+          )}
+          icon
+          type="button"
+          name="unorderedList"
+          onMouseDown={blockClicked}
+        >
+          <Icon name="list ul" />
+        </Button>
+        <Button
+          toggle
+          active={value.blocks.some(
+            (block) => value.document.getClosest(block.key, (parent) => parent.type === 'orderedList'),
+          )}
+          icon
+          type="button"
+          name="orderedList"
+          onMouseDown={blockClicked}
+        >
+          <Icon name="list ol" />
+        </Button>
+      </ButtonGroup>
     </div>
   );
 };
 
 Buttons.propTypes = {
-  buttonClicked: PropTypes.func.isRequired,
+  markClicked: PropTypes.func.isRequired,
+  blockClicked: PropTypes.func.isRequired,
+  value: PropTypes.shape({
+    activeMarks: PropTypes.object.isRequired,
+    blocks: PropTypes.object.isRequired,
+    document: PropTypes.object.isRequired,
+  }).isRequired,
 };
 
 export default Buttons;
+
+/*
+<button type="button" name="bold" onMouseDown={markClicked}>Bold</button>
+      <button type="button" name="italic" onMouseDown={markClicked}>Italic</button>
+      <button type="button" name="code" onMouseDown={markClicked}>Code</button>
+      <button type="button" name="underline" onMouseDown={markClicked}>Underline</button>
+      <button type="button" name="strikethrough" onMouseDown={markClicked}>Strikethrough</button>
+*/
